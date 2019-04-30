42703bc47fee88d219c4f6556d65b373a175ea8e
Diogo Rodrigues <diogofgr@gmail.com>
2019-04-30 16:20:25 +0100

    add index page with api docs

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,4 +2,8 @@ const noteRoutes = require('./note_routes');
 
 module.exports = function(app, db) {
     noteRoutes(app, db);
+
+    app.get('/', (req, res) => {
+        res.render('index');
+    });
 }
--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const MongoClient = require('mongodb').MongoClient;
 const bodyParser = require('body-parser');
+var path = require('path');
 require('dotenv').config();
 
 const app = express();
 const port = process.env.PORT;
 const dbURL = process.env.MONGODB_URI;
+
+// setup HTML views:
+app.set('views', path.join(__dirname, '/app/views'));
+app.set('view engine', 'pug');
+
+// setup public folder
+app.use(express.static(path.join(__dirname, '/app/public')));
 
 // to process URL encoded forms
 // (if this is missing you cannot access req.body in ./app/routes):
